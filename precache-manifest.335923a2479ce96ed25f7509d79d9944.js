e82812972720beee1c8cb5d2e796223ce0aee478
Oles Marola <oles_pw@ukr.net>
2019-12-19 16:59:15 +0200

    Updates

---
+++ precache-manifest.335923a2479ce96ed25f7509d79d9944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90167a87b816dc87263a191eff091399",
+    "url": "/react_dynamic-list-of-posts/index.html"
+  },
+  {
+    "revision": "941bcffb34aceb8be912",
+    "url": "/react_dynamic-list-of-posts/static/css/main.7f18b738.chunk.css"
+  },
+  {
+    "revision": "ae754edae905f9a43fb3",
+    "url": "/react_dynamic-list-of-posts/static/js/2.a0cf25e8.chunk.js"
+  },
+  {
+    "revision": "941bcffb34aceb8be912",
+    "url": "/react_dynamic-list-of-posts/static/js/main.120d2831.chunk.js"
+  },
+  {
+    "revision": "fc988510490290486853",
+    "url": "/react_dynamic-list-of-posts/static/js/runtime~main.69709ad2.js"
+  },
+  {
+    "revision": "cb2bd0213cb82b6309b3d20f43e81e41",
+    "url": "/react_dynamic-list-of-posts/static/media/Subtle-Prism.cb2bd021.svg"
+  }
+]);
